a3dd4e5f53a26b2eb15a7bdbe8c65de34b357961
ryanjares <jaresryan@gmail.com>
2024-11-23 14:47:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("flagCanvas");
+const ctx = canvas.getContext("2d");
+
+// Draw a blue rectangle as an example
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 200, 100);
